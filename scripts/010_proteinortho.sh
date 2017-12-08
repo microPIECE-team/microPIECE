99b29c8c8db002e6b3e5bcd28fb3fae6112a9d9f
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2017-12-08 09:49:11 +0100

    modifications of proteinortho shell script

--- scripts/010_proteinortho.sh
+++ scripts/010_proteinortho.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/bash
+#!/bin/bash
 #create folder for this step
 mkdir ../010/
 #unzip data
 zcat ../data/GCF_000002335.3_Tcas5.2_protein.faa.gz >../010/GCF_000002335.3_Tcas5.2_protein.faa
 zcat ../data/GCF_000004015.4_AaegL3_protein.faa.gz  >../010/GCF_000004015.4_AaegL3_protein.faa
 # makeblastdb
-makeblastdb ../010/GCF_000002335.3_Tcas5.2_protein.faa
-makeblastdb ../010/GCF_000004015.4_AaegL3_protein.faa
+makeblastdb -in ../010/GCF_000002335.3_Tcas5.2_protein.faa -dbtype prot
+makeblastdb -in ../010/GCF_000004015.4_AaegL3_protein.faa -dbtype prot
 # run proteinortho
-proteinortho5.pl ../010/GCF_000002335.3_Tcas5.2_protein.faa ../010/GCF_000004015.4_AaegL3_protein.faa
+proteinortho5.pl  -clean --project=../010/TCA_vs_AAE ../010/GCF_000002335.3_Tcas5.2_protein.faa ../010/GCF_000004015.4_AaegL3_protein.faa
