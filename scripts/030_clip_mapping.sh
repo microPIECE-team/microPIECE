2b3fa090552828a149cbedb507b9303e767c3b62
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2017-12-08 13:16:48 +0100

    finished 030_clip_mapping

--- scripts/030_clip_mapping.sh
+++ scripts/030_clip_mapping.sh
@@ -2,5 +2,19 @@
 command -v gsnap >/dev/null 2>&1 || { echo "I require gsnap but it's not installed. Aborting." >&2; exit 1; }
 command -v gmap_build >/dev/null 2>&1 || { echo "I require gsnap but it's not installed. Aborting." >&2; exit 1; }
 
-gmap_build -D gsnap_db -k 15 -d Aedes_aegypti GCF_000004015.4_AaegL3_genomic.fna
+mkdir ../030/
+# build genome index for gsnap
+zcat ../data/AAE/GCF_000004015.4_AaegL3_genomic.fna.gz > ../030/GCF_000004015.4_AaegL3_genomic.fna
+gmap_build -D ../030/gsnap_db -k 15 -d Aedes_aegypti ../030/GCF_000004015.4_AaegL3_genomic.fna
+
+
+# gsnap mapping
+gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163635_trim.fastq.gz > ../030/SRR5163635_trim_gsnap.sam
+gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163636_trim.fastq.gz > ../030/SRR5163636_trim_gsnap.sam
+gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163637_trim.fastq.gz > ../030/SRR5163637_trim_gsnap.sam
+
+gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163632_trim.fastq.gz > ../030/SRR5163632_trim_gsnap.sam
+gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163633_trim.fastq.gz > ../030/SRR5163633_trim_gsnap.sam
+gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163634_trim.fastq.gz > ../030/SRR5163634_trim_gsnap.sam
+
 
