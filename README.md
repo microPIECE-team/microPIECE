d36c8c1a57cee57a8037249c55a152e6f6c7bcf9
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2017-12-08 11:33:04 +0100

    upgraded 010 and 005

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tca_miRNA_data_generation
 All stuff which is required to generate data for Daniels miRNA project
+
+## 005_getFiles.sh
+Download all files needed for AGO CLIP transfer of AAE to TCA
--- scripts/010_proteinortho.sh
+++ scripts/010_proteinortho.sh
@@ -2,8 +2,8 @@
 #create folder for this step
 mkdir ../010/
 #unzip data
-zcat ../data/GCF_000002335.3_Tcas5.2_protein.faa.gz >../010/GCF_000002335.3_Tcas5.2_protein.faa
-zcat ../data/GCF_000004015.4_AaegL3_protein.faa.gz  >../010/GCF_000004015.4_AaegL3_protein.faa
+zcat ../data/TCA/GCF_000002335.3_Tcas5.2_protein.faa.gz >../010/GCF_000002335.3_Tcas5.2_protein.faa
+zcat ../data/AAE/GCF_000004015.4_AaegL3_protein.faa.gz  >../010/GCF_000004015.4_AaegL3_protein.faa
 # makeblastdb
 makeblastdb -in ../010/GCF_000002335.3_Tcas5.2_protein.faa -dbtype prot
 makeblastdb -in ../010/GCF_000004015.4_AaegL3_protein.faa -dbtype prot
