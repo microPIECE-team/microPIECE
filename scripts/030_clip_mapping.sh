a0b699ab0ae5c199cce7ad15d34a6e7f6debef00
Frank Förster <frank.foerster@ime.fraunhofer.de>
2017-12-08 13:47:00 +0100

    Add comment before folder creation

--- scripts/030_clip_mapping.sh
+++ scripts/030_clip_mapping.sh
@@ -6,9 +6,9 @@ command -v gmap_build >/dev/null 2>&1 || { echo "I require gsnap but it's not in
 command -v samtools >/dev/null 2>&1 || { echo "I require samtools but it's not installed. Aborting." >&2; exit 1; }
 command -v bedtools >/dev/null 2>&1 || { echo "I require bedtools but it's not installed. Aborting." >&2; exit 1; }
 
-
-
+# creation of temporary folder for this step
 mkdir ../030/
+
 # build genome index for gsnap
 gmap_build -g -D ../030/gsnap_db -k 15 -d Aedes_aegypti ../data/AAE/GCF_000004015.4_AaegL3_genomic.fna.gz
 
