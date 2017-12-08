970678c4d3a2ea17058ca925262ce85a308fe3cc
Frank Förster <frank.foerster@ime.fraunhofer.de>
2017-12-08 13:46:06 +0100

    Add on-the-fly convertion into sorted bam
    The conversion of sam2bam can be done on-the-fly while mapping the reads onto the genome. This will save storage space

--- scripts/030_clip_mapping.sh
+++ scripts/030_clip_mapping.sh
@@ -11,26 +11,14 @@ mkdir ../030/
 # build genome index for gsnap
 gmap_build -g -D ../030/gsnap_db -k 15 -d Aedes_aegypti ../data/AAE/GCF_000004015.4_AaegL3_genomic.fna.gz
 
+# gsnap mapping including conversion to bam to skip temporary sam-file creation
+gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163635_trim.fastq.gz | samtools view -bS - | samtools sort - ../030/SRR5163635_trim_gsnap.bam
+gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163636_trim.fastq.gz | samtools view -bS - | samtools sort - ../030/SRR5163636_trim_gsnap.bam
+gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163637_trim.fastq.gz | samtools view -bS - | samtools sort - ../030/SRR5163637_trim_gsnap.bam
 
-# gsnap mapping
-gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163635_trim.fastq.gz > ../030/SRR5163635_trim_gsnap.sam
-gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163636_trim.fastq.gz > ../030/SRR5163636_trim_gsnap.sam
-gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163637_trim.fastq.gz > ../030/SRR5163637_trim_gsnap.sam
-
-gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163632_trim.fastq.gz > ../030/SRR5163632_trim_gsnap.sam
-gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163633_trim.fastq.gz > ../030/SRR5163633_trim_gsnap.sam
-gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163634_trim.fastq.gz > ../030/SRR5163634_trim_gsnap.sam
-
-
-# sam2bam 
-samtools view -Sb ../030/SRR5163635_trim_gsnap.sam > ../030/SRR5163635_trim_gsnap.bam
-samtools view -Sb ../030/SRR5163636_trim_gsnap.sam > ../030/SRR5163636_trim_gsnap.bam
-samtools view -Sb ../030/SRR5163637_trim_gsnap.sam > ../030/SRR5163637_trim_gsnap.bam
-
-samtools view -Sb ../030/SRR5163632_trim_gsnap.sam > ../030/SRR5163632_trim_gsnap.bam
-samtools view -Sb ../030/SRR5163633_trim_gsnap.sam > ../030/SRR5163633_trim_gsnap.bam
-samtools view -Sb ../030/SRR5163634_trim_gsnap.sam > ../030/SRR5163634_trim_gsnap.bam
-
+gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163632_trim.fastq.gz | samtools view -bS - | samtools sort - ../030/SRR5163632_trim_gsnap.bam
+gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163633_trim.fastq.gz | samtools view -bS - | samtools sort - ../030/SRR5163633_trim_gsnap.bam
+gsnap --gunzip -N 1 -B 5 --speed 1 -O -A sam -D ../030/gsnap_db -d Aedes_aegypti ../020/SRR5163634_trim.fastq.gz | samtools view -bS - | samtools sort - ../030/SRR5163634_trim_gsnap.bam
 
 # bam2bed
 bedtools bamtobed -i ../030/SRR5163635_trim_gsnap.bam > ../030/SRR5163635_trim_gsnap.bed
