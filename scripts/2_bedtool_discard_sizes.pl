fd7fea60449fbadf25ebde8adbf623247e0b3b3f
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2017-12-08 15:59:59 +0100

    added 2_bedtool_discard_sizes.pl

---
+++ scripts/2_bedtool_discard_sizes.pl
@@ -0,0 +1,23 @@
+#! /usr/bin/perl
+use strict;
+use warnings;
+
+my $bedfile	= $ARGV[0];
+my $min		= $ARGV[1];	# eg 22
+my $max		= $ARGV[2];	# eg 50
+
+open(BED ,"<",$bedfile) || die;
+while(<BED>){
+	chomp;
+	my $bed_line	= $_;
+	my @bed_array	= split(" ",$bed_line);
+	my $bed_start	= $bed_array[1];
+	my $bed_stop	= $bed_array[2];
+	my $bed_len	= $bed_stop-$bed_start;
+	next if($bed_len < $min);
+	next if($bed_len > $max);
+	print "$bed_line\n";
+}
+close(BED)|| die;
+
+
