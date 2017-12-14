25a19a1aa58f5067e245ab2c4b752995bc7c3673
Frank Förster <frank.foerster@ime.fraunhofer.de>
2017-12-14 11:58:13 +0100

    Step 045 merges bed files by own script

--- scripts/045_bedtools_merge.sh
+++ scripts/045_bedtools_merge.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
-command -v bedtools >/dev/null 2>&1 || { echo "I require bedtools but it's not installed. Aborting." >&2; exit 1; }
 
-
-# merge
-bedtools merge -s -c 4,5,6 -o distinct -i ../040/SRR5163632_trim_gsnap_piranha_sort.bed > ../040/SRR5163632_trim_gsnap_piranha_sort_merge.bed
-bedtools merge -s -c 4,5,6 -o distinct -i ../040/SRR5163633_trim_gsnap_piranha_sort.bed > ../040/SRR5163633_trim_gsnap_piranha_sort_merge.bed 
-bedtools merge -s -c 4,5,6 -o distinct -i ../040/SRR5163634_trim_gsnap_piranha_sort.bed > ../040/SRR5163634_trim_gsnap_piranha_sort_merge.bed
-
-bedtools merge -s -c 4,5,6 -o distinct -i ../040/SRR5163635_trim_gsnap_piranha_sort.bed > ../040/SRR5163635_trim_gsnap_piranha_sort_merge.bed
-bedtools merge -s -c 4,5,6 -o distinct -i ../040/SRR5163636_trim_gsnap_piranha_sort.bed > ../040/SRR5163636_trim_gsnap_piranha_sort_merge.bed
-bedtools merge -s -c 4,5,6 -o distinct -i ../040/SRR5163637_trim_gsnap_piranha_sort.bed > ../040/SRR5163637_trim_gsnap_piranha_sort_merge.bed
+time perl 046_merge_bed_files.pl \
+     --input 24h_1=../040/SRR5163635_trim_gsnap_piranha_sort.bed \
+     --input 24h_2=../040/SRR5163636_trim_gsnap_piranha_sort.bed \
+     --input 24h_3=../040/SRR5163637_trim_gsnap_piranha_sort.bed \
+     --input 72h_1=../040/SRR5163632_trim_gsnap_piranha_sort.bed \
+     --input 72h_2=../040/SRR5163633_trim_gsnap_piranha_sort.bed \
+     --input 72h_3=../040/SRR5163634_trim_gsnap_piranha_sort.bed \
+     --output clip_merged.bed \
+     --log merging_bed_files.log
