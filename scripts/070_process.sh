29c6029b323dca850de17ad6d097028f28982c33
Frank Förster <frank.foerster@ime.fraunhofer.de>
2017-12-15 12:07:54 +0100

    Rename files and add executable flag

--- scripts/070_process.sh
+++ scripts/070_process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REQ_PROGS=(./2_bedtool_discard_sizes.pl bedtools ./3_fasta_uc.pl)
+REQ_PROGS=(./071_bedtool_discard_sizes.pl bedtools ./072_fasta_uc.pl)
 for prog in ${REQ_PROGS[*]}
 do
     command -v ${prog} >/dev/null 2>&1 || { echo "I require ${prog} but it's not installed. Aborting." >&2; exit 1; }
@@ -11,7 +11,7 @@ mkdir ../070/
 
 # discard sizes
 
-./2_bedtool_discard_sizes.pl ../050/clip_merged_4of6BEDfilter_mapGFF_minLen0.bed 22 50 > ../070/clip_merged_mapGFF_minLen0_4of6BEDfilter_min22_max50.bed
+./071_bedtool_discard_sizes.pl ../050/clip_merged_4of6BEDfilter_mapGFF_minLen0.bed 22 50 > ../070/clip_merged_mapGFF_minLen0_4of6BEDfilter_min22_max50.bed
 
 # sort
 sort -k1,1 -k2,2n ../070/clip_merged_mapGFF_minLen0_4of6BEDfilter_min22_max50.bed > ../070/clip_merged_mapGFF_minLen0_4of6BEDfilter_min22_max50_sort.bed
@@ -26,6 +26,6 @@ bedtools getfasta -s -name -fi \
 	 -fo ../070/clip_merged_mapGFF_minLen0_min22_max50_sort.fa
 
 # upper case
-./3_fasta_uc.pl \
+./072_fasta_uc.pl \
     ../070/clip_merged_mapGFF_minLen0_4of6BEDfilter_min22_max50_sort.fa \
     > ../070/clip_merged_mapGFF_minLen0_4of6BEDfilter_min22_max50_sort_UC.fa
