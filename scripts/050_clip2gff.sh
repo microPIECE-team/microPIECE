98768558bca9d1d0cb80b3e75d71399268422801
Frank Förster <frank.foerster@ime.fraunhofer.de>
2017-12-15 11:51:38 +0100

    Rename 1_clip_mapper.pl to 051_clip_mapper.pl

--- scripts/050_clip2gff.sh
+++ scripts/050_clip2gff.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 # mapping clip peaks to mRNAs from GFF file
-command -v ./1_clip_mapper.pl >/dev/null 2>&1 || { echo "I require 1_clip_mapper.pl but it's not installed. Aborting." >&2; exit 1; }
+command -v ./051_clip_mapper.pl >/dev/null 2>&1 || { echo "I require 1_clip_mapper.pl but it's not installed. Aborting." >&2; exit 1; }
 mkdir ../050/
 
 #copy gff file
 zcat ../data/AAE/GCF_000004015.4_AaegL3_genomic.gff.gz > ../050/GCF_000004015.4_AaegL3_genomic.gff
 
 # run mapping of peaks to gff mRNAs
-./1_clip_mapper.pl ../040/clip_merged_4of6BEDfilter.bed ../050/GCF_000004015.4_AaegL3_genomic.gff 0 > ../050/clip_merged_4of6BEDfilter_mapGFF_minLen0.bed
+./051_clip_mapper.pl ../040/clip_merged_4of6BEDfilter.bed ../050/GCF_000004015.4_AaegL3_genomic.gff 0 > ../050/clip_merged_4of6BEDfilter_mapGFF_minLen0.bed
  
