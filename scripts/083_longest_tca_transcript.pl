19fee1954638c02502f0854f2582f6d3de3ff2af
Frank Förster <frank.foerster@ime.fraunhofer.de>
2017-12-15 14:30:03 +0100

    Longest transcript for TCA

--- scripts/083_longest_tca_transcript.pl
+++ scripts/083_longest_tca_transcript.pl
@@ -8,34 +8,34 @@ my %h = ();
 while (<>)
 {
     chomp;
-    
-    my @F = split("\t", $_);
-    
-    next unless ($F[2] =~ /mRNA|exon|CDS/);
 
-    $F[8] =~ /Dbxref=VectorBase:([^-]+)-[RP]([^,]+).+Genbank:([^,]+)/;
+    my @fields = split("\t", $_);
 
-    if ($F[2] eq "mRNA")
-    { 
-	$h{$1}{$2} = {}; 
+    next unless ($fields[2] =~ /mRNA|exon|CDS/);
+
+    $fields[8] =~ /ID=([^;]+).*Dbxref=GeneID:(\d+).*Genbank:([^,;]+)/;
+
+    if ($fields[2] eq "mRNA")
+    {
+	$h{$2}{$1} = {};
     }
-    elsif ($F[2] eq "exon" && exists $h{$1}{$2} ) 
-    { 
-	$h{$1}{$2}{exon}{name} = $3; 
-	$h{$1}{$2}{exon}{len} += abs($F[3]-$F[4]); 
-    } 
-    elsif (exists $h{$1}{$2})
-    { 
-	$h{$1}{$2}{cds}{name} = $3; 
-    } 
-    else 
-    { 
-	warn "For line # $.('$_') no mRNA was found\n"; 
-    } 
+    elsif ($fields[2] eq "exon" && exists $h{$2}{$1} )
+    {
+	$h{$2}{$1}{exon}{name} = $3;
+	$h{$2}{$1}{exon}{len} += abs($fields[3]-$fields[4]);
+    }
+    elsif (exists $h{$2}{$1})
+    {
+	$h{$2}{$1}{cds}{name} = $3;
+    }
+    else
+    {
+	warn "For line # $.('$_') no mRNA was found\n";
+    }
 }
 
 foreach my $vec(sort keys %h)
-{ 
+{
     my ($longest) = sort {$h{$vec}{$b}{exon}{len} <=> $h{$vec}{$a}{exon}{len}} (keys %{$h{$vec}});
 
     next unless (exists $h{$vec}{$longest}{exon} && exists $h{$vec}{$longest}{cds});
