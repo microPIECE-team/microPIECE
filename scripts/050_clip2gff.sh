25b4d8ba7d0e4f59a8ea56ecbc4dd11a05b702bb
Frank Förster <frank.foerster@ime.fraunhofer.de>
2017-12-15 11:52:01 +0100

    Use mkdir -p to avoid error messages or warnings

--- scripts/050_clip2gff.sh
+++ scripts/050_clip2gff.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # mapping clip peaks to mRNAs from GFF file
 command -v ./051_clip_mapper.pl >/dev/null 2>&1 || { echo "I require 1_clip_mapper.pl but it's not installed. Aborting." >&2; exit 1; }
-mkdir ../050/
+mkdir -p ../050/
 
 #copy gff file
 zcat ../data/AAE/GCF_000004015.4_AaegL3_genomic.gff.gz > ../050/GCF_000004015.4_AaegL3_genomic.gff
