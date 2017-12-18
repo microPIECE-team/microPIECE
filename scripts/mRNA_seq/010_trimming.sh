a4b39f2f26236eddbc90423625129c4a4a1cd0ca
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2017-12-18 16:01:48 +0100

    changed paths

--- scripts/mRNA_seq/010_trimming.sh
+++ scripts/mRNA_seq/010_trimming.sh
@@ -4,18 +4,18 @@ command -v cutadapt >/dev/null 2>&1 || { echo "I require cutadapt but it's not i
 
 mkdir -p data/010_trimmed_RNA
             
-cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/oocytes_RNAseq_rep1_1_trim.fastq.gz data/000_raw_RNA/oocytes_RNAseq_rep1_1.fastq.gz
-cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/oocytes_RNAseq_rep1_2_trim.fastq.gz data/000_raw_RNA/oocytes_RNAseq_rep1_2.fastq.gz
-cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/oocytes_RNAseq_rep2_1_trim.fastq.gz data/000_raw_RNA/oocytes_RNAseq_rep2_1.fastq.gz
-cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/oocytes_RNAseq_rep2_2_trim.fastq.gz data/000_raw_RNA/oocytes_RNAseq_rep2_2.fastq.gz
+cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/oocytes_RNAseq_rep1_1_trim.fastq.gz data/000_mRNA_raw/oocytes_RNAseq_rep1_1.fastq.gz
+cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/oocytes_RNAseq_rep1_2_trim.fastq.gz data/000_mRNA_raw/oocytes_RNAseq_rep1_2.fastq.gz
+cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/oocytes_RNAseq_rep2_1_trim.fastq.gz data/000_mRNA_raw/oocytes_RNAseq_rep2_1.fastq.gz
+cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/oocytes_RNAseq_rep2_2_trim.fastq.gz data/000_mRNA_raw/oocytes_RNAseq_rep2_2.fastq.gz
 
-cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/Fifth-instar_S4_L001_R1_001_trim.fastq.gz Fifth-instar_S4_L001_R1_001.fastq.gz
-cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/Fifth-instar_S4_L002_R1_001_trim.fastq.gz Fifth-instar_S4_L002_R1_001.fastq.gz
-cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/Fifth-instar_S4_L003_R1_001_trim.fastq.gz Fifth-instar_S4_L003_R1_001.fastq.gz
-cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/Fifth-instar_S4_L004_R1_001_trim.fastq.gz Fifth-instar_S4_L004_R1_001.fastq.gz
+cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/Fifth-instar_S4_L001_R1_001_trim.fastq.gz data/000_mRNA_raw/Fifth-instar_S4_L001_R1_001.fastq.gz
+cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/Fifth-instar_S4_L002_R1_001_trim.fastq.gz data/000_mRNA_raw/Fifth-instar_S4_L002_R1_001.fastq.gz
+cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/Fifth-instar_S4_L003_R1_001_trim.fastq.gz data/000_mRNA_raw/Fifth-instar_S4_L003_R1_001.fastq.gz
+cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/Fifth-instar_S4_L004_R1_001_trim.fastq.gz data/000_mRNA_raw/Fifth-instar_S4_L004_R1_001.fastq.gz
  
-cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/last-larval-instar_S1_L001_R1_001_trim.fastq.gz last-larval-instar_S1_L001_R1_001.fastq.gz
-cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/last-larval-instar_S1_L002_R1_001_trim.fastq.gz last-larval-instar_S1_L002_R1_001.fastq.gz
-cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/last-larval-instar_S1_L003_R1_001_trim.fastq.gz last-larval-instar_S1_L003_R1_001.fastq.gz
-cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/last-larval-instar_S1_L004_R1_001_trim.fastq.gz last-larval-instar_S1_L004_R1_001.fastq.gz
+cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/last-larval-instar_S1_L001_R1_001_trim.fastq.gz data/000_mRNA_raw/last-larval-instar_S1_L001_R1_001.fastq.gz
+cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/last-larval-instar_S1_L002_R1_001_trim.fastq.gz data/000_mRNA_raw/last-larval-instar_S1_L002_R1_001.fastq.gz
+cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/last-larval-instar_S1_L003_R1_001_trim.fastq.gz data/000_mRNA_raw/last-larval-instar_S1_L003_R1_001.fastq.gz
+cutadapt -a AGATCGGAAGAGC -o data/010_trimmed_RNA/last-larval-instar_S1_L004_R1_001_trim.fastq.gz data/000_mRNA_raw/last-larval-instar_S1_L004_R1_001.fastq.gz
 
