0c8a095a5b282d142e5160037fa510626712355f
Frank Förster <frank.foerster@ime.fraunhofer.de>
2017-12-18 12:55:06 +0100

    Reducing the fasta header to transcript names

--- scripts/072_fasta_uc.pl
+++ scripts/072_fasta_uc.pl
@@ -5,17 +5,46 @@ use warnings;
 my $fasta_file	= $ARGV[0];
 my $counter	= 0;
 open(FA,"<",$fasta_file) || die;
+
+my @header = ();
+my $seq = "";
+
 while(<FA>){
 	chomp;
 	my $fa_line	= $_;
-	if(/^>/){
-		print "$fa_line-$counter\n";
-		$counter += 1;
-	}
-	else{
-		my $fa_line	= $_;
-		$fa_line	= uc($fa_line);
-		print "$fa_line\n";
+
+	if($fa_line =~ /^>/ || eof(FA) )
+	{
+	    if (eof(FA) && $fa_line !~ /^>/)
+	    {
+		$seq .= $_;
+	    }
+
+	    # print one sequence per header
+	    if ($seq)
+	    {
+		foreach my $head (@header)
+		{
+		    printf ">%s-%d\n%s\n", $head, ++$counter, uc($seq);
+		}
+	    }
+
+	    if ($fa_line =~ /annotation=([^-:;]+)/)
+	    {
+		@header = split(",", $1);
+	    } elsif ($fa_line =~ /counts=([^-:;]+)/)
+	    {
+		print STDERR "No annotation found for line '$fa_line'! Skipping entry...\n";
+		@header = ();
+	    } else {
+		@header = ($fa_line);
+	    }
+
+	    $seq = "";
+
+	} else {
+	    $seq .= $_;
 	}
 }
+
 close(FA) || die;
