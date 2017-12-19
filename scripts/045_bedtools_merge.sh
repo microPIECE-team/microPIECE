c0417d7d413c1fabf85d6369067eee798e819a18
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2017-12-19 08:37:51 +0100

    045 added check for installed perl modules

--- scripts/045_bedtools_merge.sh
+++ scripts/045_bedtools_merge.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+# check for required programs
+command -v perl -e 'Pod::Usage' >/dev/null 2>&1 || { echo "I require the perl module Pod::Usage but it's not installed. Aborting." >&2; exit 1; }
+command -v perl -e 'Log::Log4perl qw(:easy)' >/dev/null 2>&1 || { echo "I require the perl module Log::Log4perl qw(:easy) but it's not installed. Aborting." >&2; exit 1; }
+command -v perl -e 'Getopt::Long' >/dev/null 2>&1 || { echo "I require the perl module Getopt::Long but it's not installed. Aborting." >&2; exit 1; }
+command -v perl -e 'Data::Dumper;' >/dev/null 2>&1 || { echo "I require the perl module Data::Dumper but it's not installed. Aborting." >&2; exit 1; }
+
+
 
 time perl 046_merge_bed_files.pl \
      --input 24h_1=../040/SRR5163635_trim_gsnap_piranha_sort.bed \
