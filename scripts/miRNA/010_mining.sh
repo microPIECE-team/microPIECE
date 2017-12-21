e74402f4ba314983dd545f591607b427fd3c0d85
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2017-12-21 14:45:28 +0100

    010 mv all files to output folder

--- scripts/miRNA/010_mining.sh
+++ scripts/miRNA/010_mining.sh
@@ -16,11 +16,23 @@ mkdir -p data/003_concat_smRNA/
 cat data/002_filter_smRNA/* > data/003_concat_smRNA/TCA_smallRNA_concat.fastq
 
 
-mkdir -p output_bwa
+#mkdir -p output_bwa
 mkdir -p output_bwt1
 
-./011_miRDeep2_bwa.pl data/003_concat_smRNA/ output_bwa/ db/GCF_000002335.3_Tcas5.2_genomic.fna db/tca_mature_mirbase.fa db/mature.fa-no-tca.fa db/tca_precursor_mirbase.fa 100
-mv result_*.csv result-bwa.csv
+#./011_miRDeep2_bwa.pl data/003_concat_smRNA/ output_bwa/ db/GCF_000002335.3_Tcas5.2_genomic.fna db/tca_mature_mirbase.fa db/mature.fa-no-tca.fa db/tca_precursor_mirbase.fa 100
+#mv result_*.csv result-bwa.csv
 
-./012_miRDeep2_bwt1.pl data/003_concat_smRNA/ output_bwt1/ db/GCF_000002335.3_Tcas5.2_genomic.fna db/tca_mature_mirbase.fa db/mature.fa-no-tca.fa db/tca_precursor_mirbase.fa 100
+./012_miRDeep2_bwt1.pl data/003_concat_smRNA/ output_bwt1/ db/GCF_000002335.3_Tcas5.2_genomic.fna db/tca_mature_mirbase.fa db/mature.fa-no-tca.fa db/tca_precursor_mirbase.fa 110
 mv result_*.csv result-bwt1.csv
+
+#clean up all files 
+mv mapper_logs output_bwt1/
+mv mirdeep_runs output_bwt1/
+mv bowtie.log output_bwt1/ 
+mv expression* output_bwt1/
+mv miRNAs_expressed_all_samples_* output_bwt1/
+mv pdfs_* output_bwt1/
+mv dir_prepare* output_bwt1/
+mv error* output_bwt1/
+mv mirna_results* output_bwt1/
+mv result_* output_bwt1/
