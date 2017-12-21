502aa7539fc0d998656cc901237d55fb13d0302e
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2017-12-21 15:52:34 +0100

    040 adapted command line

--- scripts/miRNA/040_mirdeep2fasta.sh
+++ scripts/miRNA/040_mirdeep2fasta.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-./041_curated_mirdeep2fasta.pl result-bwa-bwt1.csv high-conf
+./041_curated_mirdeep2fasta.pl result-bwt1.csv 10
 
 # create miRNA mature dataset : miRBase + missing mature arms + novels
-cat tca_mature_mirbase_completed.fa result-bwa-bwt1-mature.fa > db/tca_mature_mirbase_completed_novel.fa
+cat tca_mature_mirbase_completed.fa result-bwt1-mature.fa > db/tca_mature_mirbase_completed_novel.fa
 
 # create miRNA precursir dataset: miRBase + novels
-cat db/tca_precursor_mirbase.fa  result-bwa-bwt1-hairpin.fa > db/tca_precursor_mirbase_completed_novel.fa
+cat db/tca_precursor_mirbase.fa  result-bwt1-hairpin.fa > db/tca_precursor_mirbase_completed_novel.fa
