0909d0b37fdf9894954c55e9f77ebb7ec9d6d6d7
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2018-01-11 09:43:05 +0100

    070 added miRNA.str file to script

--- scripts/miRNA/070_isomiR.sh
+++ scripts/miRNA/070_isomiR.sh
@@ -7,23 +7,22 @@ command -v perl -e 'use RNA::HairpinFigure qw/draw/' >/dev/null 2>&1 || { echo "
 mkdir -p data/004_filterN_smRNA/
 
 
-for i in data/001_trim_smRNA ; do ./071_filter_fastq_N.pl $i > $i.filterN.fastq; done;
-mv $i data/004_filterN_smRNA/
+for i in data/001_trim_smRNA ; do FILEBASENAME=$(basename $i) ./071_filter_fastq_N.pl data/001_trim_smRNA/{$FILEBASENAME} > data/004_filterN_smRNA/{$FILEBASENAME}_filterN.fastq; done;
 
 
-wget ftp://mirbase.org/pub/mirbase/CURRENT/miRNA.str.gz db/
-./072_create_mirbase_struct.pl db/tca_precursor_mirbase_completed_novel.fa db/tca_mature_mirbase_completed_novel.fa
-mv custom.str data/070_isomiR_db/
-cp db/tca_precursor_mirbase_completed_novel.fa /data/070_isomiR_db/
+wget ftp://mirbase.org/pub/mirbase/CURRENT/miRNA.str.gz -P db/
+gunzip -f -k db/miRNA.str.gz
+./072_create_mirbase_struct.pl db/tca_precursor_mirbase_completed_novel.fa db/tca_mature_mirbase_completed_novel.fa db/miRNA.str
+rm tmp_hairpin.fa tmp_struct.rna
+
+# copy the hairpin and structure file into database folder (necessary for miraligner)
+mv custom.str data/070_isomiR_db/miRNA.str
+cp db/tca_precursor_mirbase_completed_novel.fa /data/070_isomiR_db/hairpin.fa
 
 
-mkdir -p data/070_isomiR_db/
-cp db/ data/070_isomiR_db/
-cp .str data/070_isomiR_db/
-
-mkdir -p isomiR_output/
+mkdir -p data/070_isomiR_output/
 
 # run miraligner script
-./073_seqbuster_pipe.pl data/004_filterN_smRNA/ data/070_isomiR_db/ isomiR_output/ tca
+./073_seqbuster_pipe.pl data/004_filterN_smRNA/ data/070_isomiR_db/ data/070_isomiR_output/ tca
 
 
