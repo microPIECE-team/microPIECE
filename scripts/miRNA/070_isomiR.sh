969108fb44e54fd29f4572bb65eeb00501535c41
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2018-01-12 15:50:13 +0100

    070 fixed bug

--- scripts/miRNA/070_isomiR.sh
+++ scripts/miRNA/070_isomiR.sh
@@ -9,10 +9,10 @@ command -v perl -e 'use RNA::HairpinFigure qw/draw/' >/dev/null 2>&1 || { echo "
 mkdir -p data/071_filterN_smRNA/
 mkdir -p data/072_isomiR_db/
 
-for i in data/001_trim_smRNA ; 
+for i in data/001_trim_smRNA/* ; 
 do 
 	FILEBASENAME=$(basename $i) 
-	./071_filter_fastq_N.pl data/001_trim_smRNA/${FILEBASENAME} > data/071_filterN_smRNA/${FILEBASENAME}_filterN.fastq; 
+	./071_filter_fastq_N.pl ${i} > data/071_filterN_smRNA/${FILEBASENAME}_filterN.fastq; 
 done;
 
 
@@ -29,6 +29,6 @@ cp data/041_miRDeep_completed_with_novels/tca_precursor_mirbase_completed_novel.
 mkdir -p data/073_isomiR_output/
 
 # run miraligner script
-#./073_seqbuster_pipe.pl data/071_filterN_smRNA/ data/072_isomiR_db/ data/073_isomiR_output/ tca
+./073_seqbuster_pipe.pl data/071_filterN_smRNA/ data/072_isomiR_db/ data/073_isomiR_output/ tca
 
 
