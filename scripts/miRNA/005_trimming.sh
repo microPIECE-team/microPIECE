dd7667c9157aa4739b99dc8036c8a8c2321532e9
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2018-01-15 10:34:00 +0100

    005 patched

--- scripts/miRNA/005_trimming.sh
+++ scripts/miRNA/005_trimming.sh
@@ -22,14 +22,11 @@ bwa index db/TCA_all_ncRNA_but_miR.fa_dna.fa
 # alignment of smallRNA to ncRNA
 for i in data/001_trim_smRNA/* 
 do 
-bwa aln -n 1 -o 0 -e 0 -k 1 -t 100 -f $i.ncrna.sai db/TCA_all_ncRNA_but_miR.fa_dna.fa $i
-bwa samse -f $i.sam db/TCA_all_ncRNA_but_miR.fa_dna.fa $i.ncrna.sai $i
-samtools view -b -f 4 $i.sam > $i.ncrna_ual.bam
-bedtools bamtofastq -i $i.ncrna_ual.bam -fq $i.ncrna_ual.fq
-rm -rf $i.ncrna.sai
-rm -rf $i.sam
-rm -rf $i.ncrna_ual.bam
-mv $i.ncrna_ual.fq data/002_filter_smRNA/
+bwa aln -n 1 -o 0 -e 0 -k 1 -t 100 -f "$i".ncrna.sai db/TCA_all_ncRNA_but_miR.fa_dna.fa "$i"
+bwa samse -f "$i".sam db/TCA_all_ncRNA_but_miR.fa_dna.fa "$i".ncrna.sai "$i"
+samtools view -b -f 4 "$i".sam | bedtools bamtofastq -i - -fq "$i".ncrna_ual.fq
+rm "$i".ncrna.sai "$i".sam
+mv "$i".ncrna_ual.fq data/002_filter_smRNA/
 done
 
 
