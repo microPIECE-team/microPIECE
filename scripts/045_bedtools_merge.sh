8cdb255afa32a3c71aae0339bcdece16b65bd166
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2018-01-15 08:22:12 +0100

    removed # at 045

--- scripts/045_bedtools_merge.sh
+++ scripts/045_bedtools_merge.sh
@@ -8,12 +8,12 @@ done
 
 
 
-#time perl 046_merge_bed_files.pl \
-#     --input 24h_1=../040/SRR5163635_trim_gsnap_piranha_sort.bed \
-#     --input 24h_2=../040/SRR5163636_trim_gsnap_piranha_sort.bed \
-#     --input 24h_3=../040/SRR5163637_trim_gsnap_piranha_sort.bed \
-#     --input 72h_1=../040/SRR5163632_trim_gsnap_piranha_sort.bed \
-#     --input 72h_2=../040/SRR5163633_trim_gsnap_piranha_sort.bed \
-#     --input 72h_3=../040/SRR5163634_trim_gsnap_piranha_sort.bed \
-#     --output ../040/clip_merged.bed \
-#     --log ../040/merging_bed_files.log
+time perl 046_merge_bed_files.pl \
+     --input 24h_1=../040/SRR5163635_trim_gsnap_piranha_sort.bed \
+     --input 24h_2=../040/SRR5163636_trim_gsnap_piranha_sort.bed \
+     --input 24h_3=../040/SRR5163637_trim_gsnap_piranha_sort.bed \
+     --input 72h_1=../040/SRR5163632_trim_gsnap_piranha_sort.bed \
+     --input 72h_2=../040/SRR5163633_trim_gsnap_piranha_sort.bed \
+     --input 72h_3=../040/SRR5163634_trim_gsnap_piranha_sort.bed \
+     --output ../040/clip_merged.bed \
+     --log ../040/merging_bed_files.log
