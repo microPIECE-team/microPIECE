321d282cec99ce06bf144d04cc74cd30df18fac5
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2018-01-15 11:47:08 +0100

    012 patched

--- scripts/miRNA/012_miRDeep2_bwt1.pl
+++ scripts/miRNA/012_miRDeep2_bwt1.pl
@@ -41,19 +41,38 @@ if (not $out =~/\/$/){
 my $ref_genome_no_ws	= $ref_genome;
 $ref_genome_no_ws      .= "_noWhitespace.fa";
 system("$remove_ws $ref_genome > $ref_genome_no_ws");
+if($? == -1){
+	print STDERR "Execution fail : $!\n";
+        die;
+}
+
+
 
 my $mature_ref_mir		= &RNA2DNA($mature_ref_mir_file);
 my $mature_ref_mir_no_ws	= $mature_ref_mir;
 $mature_ref_mir_no_ws	       .= "_noWhitespace.fa";
 system("$remove_ws $mature_ref_mir > $mature_ref_mir_no_ws");
+if($? == -1){
+        print STDERR "Execution fail : $!\n";
+        die;
+}
+
 my $mature_other_mir		= &RNA2DNA($mature_other_mir_file);
 my $mature_other_mir_no_ws	= $mature_other_mir;
 $mature_other_mir_no_ws	       .= "_noWhitespace.fa";
 system("$remove_ws $mature_other_mir > $mature_other_mir_no_ws");
+if($? == -1){
+        print STDERR "Execution fail : $!\n";
+        die;
+}
 my $hairpin_ref_mir		= &RNA2DNA($hairpin_ref_mir_file);
 my $hairpin_ref_mir_no_ws	= $hairpin_ref_mir;
 $hairpin_ref_mir_no_ws	       .= "_noWhitespace.fa";
 system("$remove_ws $hairpin_ref_mir > $hairpin_ref_mir_no_ws");
+if($? == -1){
+        print STDERR "Execution fail : $!\n";
+        die;
+}
 
 opendir DIR, $dir;
 my @files = grep { $_ ne '.' && $_ ne '..' } readdir DIR;       # each fastq file in array without path
@@ -62,6 +81,10 @@ closedir DIR;
 
 my $bowtie_index	= "$ref_genome_no_ws-BWT_INDX";
 system("$bowtie_build $ref_genome_no_ws $bowtie_index");
+if($? == -1){
+        print STDERR "Execution fail : $!\n";
+        die;
+}
 
 
 
@@ -69,41 +92,50 @@ foreach(@files){
 	my $file	= $_;
 	my $inpath	= $dir.$file;
 	my $outpath	= $out.$file;
-# fastq2fasta.pl fastq file > outputfile
 	my $fasta_file	= $outpath;
 	$fasta_file	=~s/\./_/g;
 	$fasta_file    .= ".fasta";
 	print "FASTQ2FASTA\n";
-	system("$fastq2fasta $inpath > $fasta_file"); 	
-# remove_white_space_in_id.pl
+	system("$fastq2fasta $inpath > $fasta_file"); 
+	if($? == -1){
+	        print STDERR "Execution fail : $!\n";
+       		die;
+	}	
 	my $fasta_no_whitespace	= $fasta_file;
 	$fasta_no_whitespace	=~s/\.fasta$/_noWhitespace.fasta/;
 	print "NO WHITESPACE\n";
 	system("$remove_ws $fasta_file > $fasta_no_whitespace");
-# collapse_reads.pl reads.fa tca
+	if($? == -1){
+	        print STDERR "Execution fail : $!\n";
+	        die;
+	}
 	my $fasta_collapse	= $fasta_no_whitespace;
 	$fasta_collapse		=~s/\.fasta$/_collapse.fasta/;
 	print "COLLAPSE\n";
 	system("$collapse_reads $fasta_no_whitespace tca > $fasta_collapse");
-
+	if($? == -1){
+       		print STDERR "Execution fail : $!\n";
+	        die;
+	}
 	# mapper (BOWTIE)
 	my $bowtie_arf	= $fasta_collapse;
 	$bowtie_arf	=~s/\.fasta$/.arf/;
 	print "MAPPER\n";
-	print "$mapper $fasta_collapse -c -q -n -l 17 -p $bowtie_index -t $bowtie_arf\n";
 	system("$mapper $fasta_collapse -c -q -n -l 17 -p $bowtie_index -t $bowtie_arf");
-
-
-
-
-
-#mirdeep
-#miRDeep2.pl reads.fa genome.fa reads_vs_genome.arf mautre_ref_miRNAs.fa mature_other_miRNAs.fa  hairpin_ref_miRNAs -t Mouse 2>report.log
+	        if($? == -1){
+                print STDERR "Execution fail : $!\n";
+                die;
+        }
 
 	my $mirdeep2_log	= $out."run.log";
 	print "MIRDEEP2\n";
 	
-	system("$mirdeep $fasta_collapse $ref_genome_no_ws $bowtie_arf $mature_ref_mir_no_ws $mature_other_mir_no_ws $hairpin_ref_mir_no_ws -P");	
+	system("$mirdeep $fasta_collapse $ref_genome_no_ws $bowtie_arf $mature_ref_mir_no_ws $mature_other_mir_no_ws $hairpin_ref_mir_no_ws -P");
+        if($? == -1){
+                print STDERR "Execution fail : $!\n";
+                die;
+        }
+	
 }
 
 
@@ -112,8 +144,8 @@ foreach(@files){
 sub RNA2DNA{
         my $r2d_rna_file        = $_[0];
         my $r2d_dna_file        = $r2d_rna_file."_dna.fa";
-        open(OUT,">",$r2d_dna_file);
-        open(R2D,"<",$r2d_rna_file);
+        open(OUT,">",$r2d_dna_file) || die;
+        open(R2D,"<",$r2d_rna_file) || die;
         while(<R2D>){
                 chomp;
                 my $r2d_line    = $_;
@@ -127,8 +159,8 @@ sub RNA2DNA{
                         print OUT "$r2d_dna_seq\n";
                 }
         }
-        close(R2D);
-        close(OUT);
+        close(R2D) || die;
+        close(OUT) || die;
         return($r2d_dna_file);
 }
 
