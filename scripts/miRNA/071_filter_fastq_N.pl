93cf97df5073aec137dcebec7e5ec1c57bc77433
Frank Förster <foersterfrank@gmx.de>
2018-01-16 11:09:03 +0100

    Update 071_filter_fastq_N.pl
    Complete refactoring of the script.

--- scripts/miRNA/071_filter_fastq_N.pl
+++ scripts/miRNA/071_filter_fastq_N.pl
@@ -2,39 +2,20 @@
 use strict;
 use warnings;
 
+# input is a FASTQ file, which will be scanned for sequences containing Ns.
+# All FASTQ blocks containing Ns will be skipped. All other are printed to STDOUT.
 
-my $fastq_file	= $ARGV[0];
-my %fastq_hash;
-my $fastq_header;
-my $fastq_count	= 0;
-open(FQ,"<",$fastq_file) || die;
-while(<FQ>){
-	chomp;
-	my $fastq_line	 = $_;
-	$fastq_count += 1;	
-	if($fastq_count == 1){
-		$fastq_header			= $fastq_line;
-		my @fastq_tmp1			= ($fastq_line);
-		$fastq_hash{$fastq_header} 	= \@fastq_tmp1;
-	}	
-	else{
-		my @fastq_tmp2			= @{$fastq_hash{$fastq_header}};
-		push(@fastq_tmp2,$fastq_line);
-		$fastq_hash{$fastq_header}	= \@fastq_tmp2;
-	}
-	if($fastq_count == 4){
-		$fastq_count = 0;
-	}
-}
-close(FQ) || die;
+while (<>)
+{
+	my $header  = $_;
+	my $seq     = <> || die "wrong number of lines\n";
+	my $header2 = <> || die "wrong number of lines\n";
+	my $qual    = <> || die "wrong number of lines\n";
 
-
-foreach(keys %fastq_hash){
-	my $fastq_key	= $_;
-	my @fastq_array	= @{$fastq_hash{$fastq_key}};
-	my $fastq_n	= "N";
-	next if ( index ($fastq_array[1],$fastq_n) != -1);
-	foreach(@fastq_array){
-		print "$_\n";
-	}
+	# check for N or n in a string
+	my $num_Ns = $seq =~ tr/Nn/Nn/;
+	
+	next if ($num_Ns > 0);
+	
+	print $header, $seq, $header2, $qual;
 }
