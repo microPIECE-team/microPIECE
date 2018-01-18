c6887dbf15465cb1429da2d17356848dbc4cce94
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2018-01-18 14:07:30 +0100

    run_all.sh - removed old scripts and #

--- scripts/miRNA/run_all.sh
+++ scripts/miRNA/run_all.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#time ./005_trimming.sh &&
+time ./005_trimming.sh &&
 time ./010_mining.sh &&
 time ./020_complete_miRBase.sh &&
-time ./030_get_novel_miRs.sh &&
 time ./040_mirdeep2fasta.sh &&
-time ./050_smRNA_overview.sh &&
 time ./060_quantification.sh &&
 time ./070_isomiR.sh &&
 time ./080_miRNA_posGenome.sh 
