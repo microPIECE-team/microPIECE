054532ce71916faa770060c223943ba3944e8765
Frank Förster <frank.foerster@ime.fraunhofer.de>
2018-02-22 12:30:30 +0100

    Move welcome message to module

--- lib/microPIECE.pm
+++ lib/microPIECE.pm
@@ -2,4 +2,49 @@ package microPIECE;
 
 use version 0.77; our $VERSION = version->declare("v0.9.0");
 
+=pod
+
+Just a little welcome screen indicating our current version number
+
+=cut
+
+sub hello
+{
+    # version15char is a spacer with 15 character length
+    # the current version number is obtained and will be extended to
+    # 15 characters in total length to fit into the ASCII art
+    my $version15char = $VERSION->normal();
+    my $required_spaces = 15-length($version15char)-1;
+    $version15char .= " "x$required_spaces;
+    print "
+                    @ @ @ @ @ @
+                    @       @ @
+                    @ @   @ @ @
+          @ @ @ @ @ @ @   @ @ @ @ @ @ @
+          @ @                         @
+          @ @                         @
+  @ @ @   @ @                     @ @ @
+@ @   @ @ @ @    m i c r o      @ @
+@       @ @ @                   @
+@                P I E C E      @
+@       @ @ @                   @ @
+@ @ @ @ @ @ @    $version15char   @ @ @
+    @     @ @                         @
+          @ @                         @
+          @ @ @ @ @ @ @ @ @ @ @ @ @ @ @
+                  @ @         @ @
+                  @ @         @ @
+                  @ @ @ @ @ @ @ @
+                  @ @         @ @
+                  @ @         @ @
+                  @ @ @ @ @ @ @ @
+                  @ @         @ @
+                  @ @         @ @
+                  @ @ @ @ @ @ @ @
+                  @ @         @ @
+                  @ @         @ @
+
+"
+}
+
 1;
--- microPIECE.pl
+++ microPIECE.pl
@@ -9,8 +9,7 @@ use lib "$RealBin/lib/";
 use microPIECE;
 use Log::Log4perl;
 
-
-hello();
+microPIECE::hello();
 
 # get a logger
 my $L = Log::Log4perl::get_logger();
@@ -22,43 +21,6 @@ Log::Log4perl->init( \q(
 	log4perl.appender.Screen.layout.ConversionPattern = [%d{yy-MM-dd HH:mm:ss}] %m%n
 ));
 
-sub hello
-{
-    # version15char is a spacer with 15 character length
-    # the current version number is obtained and will be extended to
-    # 15 characters in total length to fit into the ASCII art
-    my $version15char = $microPIECE::VERSION->normal();
-    my $required_spaces = 15-length($version15char)-1;
-    $version15char .= " "x$required_spaces;
-    print "
-                    @ @ @ @ @ @         
-                    @       @ @         
-                    @ @   @ @ @         
-          @ @ @ @ @ @ @   @ @ @ @ @ @ @ 
-          @ @                         @ 
-          @ @                         @ 
-  @ @ @   @ @                     @ @ @ 
-@ @   @ @ @ @    m i c r o      @ @     
-@       @ @ @                   @       
-@                P I E C E      @       
-@       @ @ @                   @ @     
-@ @ @ @ @ @ @    $version15char   @ @ @ 
-    @     @ @                         @ 
-          @ @                         @ 
-          @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ 
-                  @ @         @ @       
-                  @ @         @ @       
-                  @ @ @ @ @ @ @ @       
-                  @ @         @ @       
-                  @ @         @ @       
-                  @ @ @ @ @ @ @ @       
-                  @ @         @ @       
-                  @ @         @ @       
-                  @ @ @ @ @ @ @ @       
-                  @ @         @ @       
-                  @ @         @ @       
-"
-}
 
 __END__
 
