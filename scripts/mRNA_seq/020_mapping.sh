6d5e3955d5ef9e2af8a92e153bc44cbbabdf2e1e
Daniel Amsel <daniel.amsel@ime.fraunhofer.de>
2018-03-02 08:53:49 +0100

    replaced tophat2 by hisat2 and reconfigured the pipeline - removed cutadapt and adjusted cufflinks

--- scripts/mRNA_seq/020_mapping.sh
+++ scripts/mRNA_seq/020_mapping.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 #get maximum threads
-NPROC="$(nproc)"
+#NPROC="$(nproc)"
 
 mkdir -p data/020_mapped_RNA
 
 gunzip db/GCF_000002335.3_Tcas5.2_genomic.fna.gz
 gunzip db/GCF_000002335.3_Tcas5.2_genomic.gff.gz
 # build genome index
-bowtie2-build db/GCF_000002335.3_Tcas5.2_genomic.fna db/TCA52_BWT2
+/opt/hisat2-2.1.0/hisat2-build --seed 1337 db/GCF_000002335.3_Tcas5.2_genomic.fna db/TCA52_HISAT2
 
+# align raw reads to genome
+/opt/hisat2-2.1.0/hisat2 --seed 1337 -p 100 --dta-cufflinks -x db/TCA52_HISAT2 -1 data/000_mRNA_raw/oocytes_RNAseq_rep1_1.fastq.gz,data/000_mRNA_raw/oocytes_RNAseq_rep2_1.fastq.gz -2 data/000_mRNA_raw/oocytes_RNAseq_rep1_2.fastq.gz,data/000_mRNA_raw/oocytes_RNAseq_rep2_2.fastq.gz -S data/020_mapped_RNA/oocyte.sam
 
-tophat2 -p $NPROC -o data/020_mapped_RNA/oocyte/ -G db/GCF_000002335.3_Tcas5.2_genomic.gff db/TCA52_BWT2 data/010_trimmed_RNA/oocytes_RNAseq_rep1_1_trim.fastq.gz,data/010_trimmed_RNA/oocytes_RNAseq_rep2_1_trim.fastq.gz data/010_trimmed_RNA/oocytes_RNAseq_rep1_2_trim.fastq.gz,data/010_trimmed_RNA/oocytes_RNAseq_rep2_2_trim.fastq.gz
+/opt/hisat2-2.1.0/hisat2 --seed 1337 -p 100 --dta-cufflinks -x db/TCA52_HISAT2 -U data/000_mRNA_raw/Fifth-instar_S4_L001_R1_001.fastq.gz,data/000_mRNA_raw/Fifth-instar_S4_L002_R1_001.fastq.gz,data/000_mRNA_raw/Fifth-instar_S4_L003_R1_001.fastq.gz,data/000_mRNA_raw/Fifth-instar_S4_L004_R1_001.fastq.gz -S data/020_mapped_RNA/L5.sam
 
-tophat2 -p $NPROC -o data/020_mapped_RNA/L5/ -G db/GCF_000002335.3_Tcas5.2_genomic.gff db/TCA52_BWT2 data/010_trimmed_RNA/Fifth-instar_S4_L001_R1_001_trim.fastq.gz,data/010_trimmed_RNA/Fifth-instar_S4_L002_R1_001_trim.fastq.gz,data/010_trimmed_RNA/Fifth-instar_S4_L003_R1_001_trim.fastq.gz,data/010_trimmed_RNA/Fifth-instar_S4_L004_R1_001_trim.fastq.gz
+/opt/hisat2-2.1.0/hisat2 --seed 1337 -p 100 --dta-cufflinks -x db/TCA52_HISAT2 -U data/000_mRNA_raw/last-larval-instar_S1_L001_R1_001.fastq.gz,data/000_mRNA_raw/last-larval-instar_S1_L002_R1_001.fastq.gz,data/000_mRNA_raw/last-larval-instar_S1_L003_R1_001.fastq.gz,data/000_mRNA_raw/last-larval-instar_S1_L004_R1_001.fastq.gz -S data/020_mapped/LLI.sam
 
-tophat2 -p $NPROC -o data/020_mapped_RNA/LLI/ -G db/GCF_000002335.3_Tcas5.2_genomic.gff db/TCA52_BWT2 data/010_trimmed_RNA/last-larval-instar_S1_L001_R1_001_trim.fastq.gz,data/010_trimmed_RNA/last-larval-instar_S1_L002_R1_001_trim.fastq.gz,data/010_trimmed_RNA/last-larval-instar_S1_L003_R1_001_trim.fastq.gz,data/010_trimmed_RNA/last-larval-instar_S1_L004_R1_001_trim.fastq.gz
+samtools view -bS -@ 100 data/020_mapped_RNA/oocyte.sam | samtools sort -@ 100 - -o data/020_mapped_RNA/oocyte.bam
+samtools view -bS -@ 100 data/020_mapped_RNA/L5.sam | samtools sort -@ 100 - -o data/020_mapped_RNA/L5.bam
+samtools view -bS -@ 100 data/020_mapped/LLI.sam | samtools sort -@ 100 - -o data/020_mapped/LLI.bam
+
--- scripts/mRNA_seq/030_cuffdiff.sh
+++ scripts/mRNA_seq/030_cuffdiff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p data/030_cuffdiff/ 
-cuffdiff -o data/030_cuffdiff/ -L oocyte,L5,LLI db/GCF_000002335.3_Tcas5.2_genomic.gff data/020_mapped_RNA/oocyte/accepted_hits.bam data/020_mapped_RNA/L5/accepted_hits.bam data/020_mapped_RNA/LLI/accepted_hits.bam
+cuffdiff -o data/030_cuffdiff/ -L oocyte,L5,LLI db/GCF_000002335.3_Tcas5.2_genomic.gff data/020_mapped_RNA/oocyte.bam data/020_mapped_RNA/L5.bam data/020_mapped_RNA/LLI.bam
