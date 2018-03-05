3f465642eeab28a645e8526cd7f17243c7da831a
Frank Förster <frank.foerster@ime.fraunhofer.de>
2018-03-05 14:42:19 +0100

    Fix BED file sorting
    Fix #63
    
    Sorting was performed wrong, but result was correct. Nevertheless, it is corrected.

--- lib/microPIECE.pm
+++ lib/microPIECE.pm
@@ -1004,7 +1004,7 @@ sub run_CLIP_process
 	    push(@dat, \@fields);
 	}
 
-	@dat = sort { $a->[0] cmp $b->[0] || $a->[1] <=> $b->[2] } (@dat);
+	@dat = sort { $a->[0] cmp $b->[0] || $a->[1] <=> $b->[1] } (@dat);
 
 	open(FH, ">", $sorted_bed) || $L->logdie("Unable to open '$sorted_bed': $!");
 	foreach my $fields (@dat)
