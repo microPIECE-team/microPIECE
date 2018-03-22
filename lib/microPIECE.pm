27c1db32ad1f73be7f16b282aa9d8e8047f5c6b1
Frank Förster <frank.foerster@ime.fraunhofer.de>
2018-03-22 13:58:19 +0100

    Bump to version v1.2.0

--- lib/microPIECE.pm
+++ lib/microPIECE.pm
@@ -3,7 +3,7 @@ package microPIECE;
 use strict;
 use warnings;
 
-use version 0.77; our $VERSION = version->declare("v1.1.0");
+use version 0.77; our $VERSION = version->declare("v1.2.0");
 
 use Log::Log4perl;
 use Data::Dumper;
